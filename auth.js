c2fc99fefcff8828054a0d60419ad00e7ac1e46f
Andrew Pratt <Andrew.Pratt@Ticketmaster.com>
2016-06-03 15:21:23 -0700

    basic file structure and functionality, testing it out publicly

---
+++ auth.js
@@ -0,0 +1,59 @@
+var fs = require('fs');
+var apiKeysFileLocation = __dirname + '/applicationKeys.json';
+
+var allowKeyFileUpdating = false;
+var publicPaths = [];
+
+module.exports = function(express, app, config) {
+    publicPaths = config.publicPaths || [];
+    allowKeyFileUpdating = config.allowKeyFileUpdating || false;
+
+    require(__dirname+'/configurationManager')(express, app, apiKeysFileLocation, fs);
+    return auth;
+};
+
+function auth(req, res, next){
+    var i = 0;
+    for (i; i < publicPaths.length; i++) {
+        var regex = regexify(publicPaths[i]);
+        if (regex.test(`${req.method}:${req.path}`)) {
+            return next();
+        }
+    }
+    var requestKey = req.headers['x-api-key'];
+    if (!requestKey) return res.sendStatus(401);
+
+    fs.readFile(apiKeysFileLocation, (err, data) => {
+        if (err) throw err;
+        var applicationKeys = JSON.parse(data.toString());
+        if (isValid(requestKey, applicationKeys, req)) return next();
+        return res.sendStatus(401);
+    });
+}
+
+function isValid(key, keys, req) {
+    var path = req.path;
+    if (path.charAt(path.length-1) == '/') path = path.substring(0, path.length-1);
+    var permissionInfo = keys[key];
+    if (!permissionInfo) return false;
+    if (permissionInfo.ips.indexOf(req.ip) < 0) permissionInfo.ips.push(req.ip);
+    var validRoutes = permissionInfo.routes;
+    var i = 0;
+    for (i; i < validRoutes.length; i++) {
+        var regex = regexify(validRoutes[i]);
+        if (regex.test(`${req.method}:${path}`)) {
+            permissionInfo.counts[i]++;
+            if (allowKeyFileUpdating) fs.writeFile(apiKeysFileLocation, JSON.stringify(keys, null, 4));
+            if (!permissionInfo.locked) return true;
+            break;
+        }
+    }
+    return false;
+}
+
+function regexify(path) {
+    var regexstr = path.replace(/\/:.*?\/\s?/, '\/(?:([^\/]+?))\/');
+    regexstr = path.replace(/\/:.*?.*\s?/, '\/(?:([^\/]+?))');
+    regexstr += '\/?$';
+    return new RegExp(regexstr);
+}
---
+++ configurationManager.js
@@ -0,0 +1,90 @@
+module.exports = function(express, app, apiKeysFileLocation, fs) {
+    var sessionExpireation = 1800000; //30min
+    var session = {
+        key: '',
+        expires: 0
+    };
+
+    app.use('/andrewsKeyManagementTool', express.static(__dirname + '/public'));
+
+    app.get('/andrewsKeyManagementTool/admin',function(req, res){
+        var loginPage = fs.readFileSync(__dirname + '/public/login.html').toString();
+        return res.send( loginPage );
+    });
+
+    app.post('/andrewsKeyManagementTool/login',function(req, res){
+        // use crypto here, add multiple users, check for default and force change
+        var user = JSON.parse(fs.readFileSync(__dirname + '/user.json').toString());
+        if (user.username == req.body.username && user.password == req.body.password) {
+            session.key = randomString(25);
+            session.expires  = Date.now() + sessionExpireation;
+            return res.redirect( '/andrewsKeyManagementTool/admin/redirected?id='+session.key );
+        } else {
+            return res.sendStatus(401);
+        }
+    });
+
+    app.get('/andrewsKeyManagementTool/admin/redirected',function(req, res){
+        if (req.query.id === session.key && Date.now() <= session.expires) {
+        // if (true) {
+            session.expires  = Date.now() + sessionExpireation;
+            var configurationPage = fs.readFileSync(__dirname + '/public/configuration.html').toString();
+
+            var applicationKeys = JSON.parse(fs.readFileSync(apiKeysFileLocation).toString());
+            var keysBuilder = [];
+            var i = 0, j = 0;
+            for (var apiKey in applicationKeys) {
+                keysBuilder.push(
+                `<li>
+                    <div class="key-name">${apiKey}</div>
+                    <div class="key-status">locked: ${applicationKeys[apiKey].locked}</div>
+                    <div class="key-ips">${applicationKeys[apiKey].ips.join(', ')}</div>
+                    <ul>`
+                );
+                for (i; i < applicationKeys[apiKey].routes.length; i++) {
+                    keysBuilder.push(
+                        `<li>
+                            <div class="key-route">${applicationKeys[apiKey].routes[i]}</div>
+                            <div class="key-count">${applicationKeys[apiKey].counts[i]}</div>
+                        </li>`
+                    );
+                }
+                keysBuilder.push(
+                    `</ul>
+                </li>`
+                );
+            }
+            var routes = app._router.stack;
+            var routesBuilder = [];
+            i = 0, j = 0;
+            for (i; i < routes.length; i++) {
+                if (routes[i].route) {
+                    j = 0;
+                    for (j; j < routes[i].route.stack.length; j++) {
+                        routesBuilder.push(
+                        `<li>
+                            <div class="routes-method">${routes[i].route.stack[j].method}</div>
+                            <div class="routes-path">${routes[i].route.path}</div>
+                        </li>`
+                        );
+                    }
+                }
+            }
+            configurationPage = configurationPage.replace( '<^_^>', keysBuilder.join(''));
+            configurationPage = configurationPage.replace( '<O.o>', routesBuilder.join(''));
+            return res.send( configurationPage );
+        } else {
+            return res.sendStatus(401);
+        }
+    });
+}
+
+function randomString(length, characters) {
+    var text = '';
+    characters = characters || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    length = length || 25;
+
+    for( var i=0; i < length; i++ )
+        text += characters.charAt(Math.floor(Math.random() * characters.length));
+    return text;
+}
